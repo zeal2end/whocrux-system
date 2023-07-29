223f1d49318f346ef7a1d2089c78b6033677a345
Vivek Kumar <vyvivekkumar@gmail.com>
2023-07-29 10:30:52 +0530

    test Home app

--- Home/urls.py
+++ Home/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "Home"
 urlpatterns = [
-    path("", views.index, name='index'),
+    path("", views.IndexView.as_view(), name='index'),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/result/", views.ResultView.as_view(), name="result"),
+    path("<int:question_id>/vote/", views.vote, name="vote")
 ]
--- Home/views.py
+++ Home/views.py
@@ -1,8 +1,35 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question, Choice
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
+class IndexView(generic.ListView):
+    template_name = "Home/index.html"
+    context_object_name = "latest_question_list"
 
-def index(request):
-    return HttpResponse("Hello, world. You are at home page!!")
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "Home/detail.html"
 
-# Create your views here.
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = "Home/result.html"
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "Home/detail.html", {
+            "question": question,
+            "error_message": "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("Home:result", args=(question.id,)))
+
